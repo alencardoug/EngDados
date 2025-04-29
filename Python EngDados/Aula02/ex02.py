a16062c4a14c8cef9f5f1567403fc43dafd6c95e
alencardoug <alencardoug@gmail.com>
2025-04-29 19:08:08 -0300

    Finalizado e testado ex02.py

--- Python EngDados/Aula02/ex02.py
+++ Python EngDados/Aula02/ex02.py
@@ -1,1 +1,12 @@
-#Codigo a desenvolver
+# 2. Crie um programa que receba um número do usuário e calcule
+# o resto da divisão desse número por 5.
+tipo_numero01 = str
+while tipo_numero01 != int:
+    numero01 = input('Digite o número do usuario: ')
+    try:
+        tipo_numero01 = type(int(numero01))
+    except ValueError:
+        print('Somente número inteiro.')
+numero01 = int(numero01)
+resto = numero01 % 5
+print(resto)
