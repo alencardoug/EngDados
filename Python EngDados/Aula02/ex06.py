10c3af23f733b3a7f11f3988082baabd56924f5f
alencardoug <alencardoug@gmail.com>
2025-04-30 02:33:56 -0300

    criado branch ex06_10 e finalizado ex06.py

---
+++ Python EngDados/Aula02/ex06.py
@@ -0,0 +1,27 @@
+# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
+# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# 8. Desenvolva um programa que calcule a potência de um número (base e expoente
+#  fornecidos pelo usuário).
+# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio
+#  como entrada.
+
+# 6
+tipo_numero01 = str
+while tipo_numero01 != float:
+    numero01 = input('Digite o 1o número: ')
+    try:
+        tipo_numero01 = type(float(numero01))
+    except ValueError:
+        print('Somente com decimal.')
+
+tipo_numero02 = str
+while tipo_numero02 != float:
+    numero02 = input('Digite o 2o número: ')
+    try:
+        tipo_numero02 = type(float(numero02))
+    except ValueError:
+        print('Somente com decimal.')
+
+soma = float(numero01) + float(numero02)
+print(f'Número {numero01} somando ao {numero02} soma {soma}.')
