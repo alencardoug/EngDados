81baee2d10c2e52b790c8cd1b9e04f5a53e9ce48
alencardoug <alencardoug@gmail.com>
2025-04-30 00:51:27 -0300

    ex03 finalizado e testado

--- Python EngDados/Aula02/ex03.py
+++ Python EngDados/Aula02/ex03.py
@@ -1,1 +1,20 @@
-# Iniciado resulução ex03
+# 3. Desenvolva um programa que multiplique dois números
+# fornecidos pelo usuário e mostre o resultado.
+tipo_numero01 = str
+while tipo_numero01 != float:
+    numero01 = input('Digite o 1o número: ')
+    try:
+        tipo_numero01 = type(float(numero01))
+    except ValueError:
+        print('Somente número.')
+
+tipo_numero02 = str
+while tipo_numero02 != float:
+    numero02 = input('Digite o 2o número: ')
+    try:
+        tipo_numero02 = type(float(numero02))
+    except ValueError:
+        print('Somente número.')
+
+produto = float(numero01) * float(numero02)
+print(f'Número {numero01} multiplicado com {numero02} é {produto}.')
