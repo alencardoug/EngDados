8c3019d022561c3b589dda6d669d4287bcb6a6fe
Douglas Alencar <110272528+alencardoug@users.noreply.github.com>
2025-04-30 21:32:17 -0300

    feat: desenvolve 2 solucoes, via 3 arquivos codados pelo chat gpt (#8)
    * ex06.py
    
    * feat: desenvolve desafio, 2 pelo chat gpt, 1 modularizado em utils.py e desafio_mod_gpt.py e 1 inteiro em um arquivo desafio_gpt.py

--- Python EngDados/Aula02/ex06.py
+++ Python EngDados/Aula02/ex06.py
@@ -1,5 +1,6 @@
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+# 7. Crie um programa que calcule a média de dois números flutuantes
+#  fornecidos pelo usuário.
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente
 #  fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
@@ -7,6 +8,28 @@
 #  como entrada.
 
 # 6
+print(' Programa que soma números float. ')
+tipo_numero01 = str
+while tipo_numero01 != float:
+    numero01 = input('Digite o 1o número: ')
+    try:
+        tipo_numero01 = type(float(numero01))
+    except ValueError:
+        print('Somente com decimal.')
+
+tipo_numero02 = str
+while tipo_numero02 != float:
+    numero02 = input('Digite o 2o número: ')
+    try:
+        tipo_numero02 = type(float(numero02))
+    except ValueError:
+        print('Somente com decimal.')
+
+soma = float(numero01) + float(numero02)
+print(f'Número {numero01} somando ao {numero02} soma {soma}.')
+
+# 7
+print(' Programa que faz a média de números float. ')
 tipo_numero01 = str
 while tipo_numero01 != float:
     numero01 = input('Digite o 1o número: ')
